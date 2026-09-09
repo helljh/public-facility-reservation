003123a03c77795e1c99cff79d33621619aea4fc
helljh <junho308917@naver.com>
2026-09-09 14:46:57 +0900

    chore: initialize project

---
+++ backend/src/main/java/com/helljh/publicfacility/PublicfacilityApplication.java
@@ -0,0 +1,13 @@
+package com.helljh.publicfacility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicfacilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicfacilityApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/helljh/publicfacility/PublicfacilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helljh.publicfacility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicfacilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
